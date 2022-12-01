627512b73c39b68926231392fccc1b8f1f3a0774
run-qiao <809732792@qq.com>
2022-12-01 17:17:30 +0800

    fix some name bug

--- tracking/analysis_results.py
+++ tracking/analysis_results.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 
 prj_path = os.path.join(os.path.dirname(__file__), '..')
 if prj_path not in sys.path:
@@ -14,9 +15,21 @@ from lib.test.tracker import trackerlist
 
 trackers = []
 
-trackers.extend(trackerlist(name='cttrack_online', parameter_name='mixattn', dataset_name='lasot',
-                            run_ids=0, display_name='cttrack_online'))
-dataset = get_dataset('lasot')
-print_results(trackers, dataset, 'lasot', merge_results=False, plot_types=('success', 'prec', 'norm_prec'))
+def parse_args():
+    parser = argparse.ArgumentParser(description='Parse args for analysis')
+    # for analysis
+    parser.add_argument('--script', type=str, default='cttrack',help='training script name')
+    parser.add_argument('--config', type=str, default='baseline', help='yaml configure file name')
+    parser.add_argument('--dataset_name', type=str, default='lasot',help='dataset of analysis')
+    args = parser.parse_args()
+
+    return args
+
+args=parse_args()
+
+trackers.extend(trackerlist(name=args.script, parameter_name=args.config, dataset_name=args.dataset_name,
+                            run_ids=0, display_name=args.script))
+dataset = get_dataset(args.dataset_name)
+print_results(trackers, dataset, args.dataset_name, merge_results=False, plot_types=('success', 'prec', 'norm_prec'))
 print_per_sequence_results(trackers, dataset, report_name="debug")
 
